293d9ed772025f86e5fb2c1c17c8a5a90ff3d6ac
ayush14623 <ayush146235@gmail.com>
2016-10-01 02:16:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,12 @@ ele.innerHTML= 'new';
 
 // move img
 var img=document.getElementById('img1');
+function moveRight(){
+    marginLeft =marginLeft +5;
+    img.style.marginLeft= marginLeft+'px';
+}
+
 img.onclick =function(){
+    var interval=setInterval(moveRight,100);
     img.style.marginLeft= '100px';
 }
